91757aa8948c4c963c8af922fe6a09f591af4de0
Aniketh <a.gondesi@student.saedubai.com>
2019-08-15 00:37:33 +0400

    First person Cam and movement mechanics.

---
+++ Assets/Scripts/FPSCameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCameraController : MonoBehaviour
+{
+    [SerializeField] float sensitivity;            // The speed at which the camera will move.
+    float mouseX;                                  // The value the mouse is moved on the X axis 
+    float mouseY;                                  // The value the mouse is moved on the Y axis
+    [SerializeField] float mouseYMin;              // Minimum of Y axis clipping
+    [SerializeField] float mouseYMax;              //Maximum of Y axis clipping
+
+
+    void LateUpdate()
+    {
+        mouseX += Input.GetAxis("Mouse X") * sensitivity;                 // Getting the rotaion value around Y axis by adding the mouse inutp position in the game world.
+        mouseY -= Input.GetAxis("Mouse Y") * sensitivity;                 // Getting the rotaion value around X axis by adding the mouse inutp position in the game world.
+        mouseY = Mathf.Clamp(mouseY, mouseYMin, mouseYMax);               // Clipping Y values so that the player can't virtically look in 360 degrees
+
+        transform.rotation = Quaternion.Euler(0, mouseX, 0);                              // Rotating player with change in mouse X value
+        Camera.main.transform.rotation = Quaternion.Euler(mouseY, mouseX, 0);             // Rotationg camera with change in mouse X and Y values
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float normalSpeed;                    // The normal speed the player will move in the game
+    [SerializeField] float sprintMultipler;                // The number multiplied to normal speed to give sprint speed
+    [SerializeField] int jumpForce;                        // The velocity increase in Y axis to perform jump
+    float sprintSpeed;                                     // The speed at which player sprints in the game
+    float speed;                                           // The current speed of the player
+    Vector3 keyboardInput;                                 // The movemnet inputs from the keyboard (set direction to move)
+    Rigidbody playerRb;                                    // rigid body of the player
+    [SerializeField] bool grounded = true;                 // checking if player on the ground
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        sprintSpeed = normalSpeed * sprintMultipler;       // Setting sprint speed
+    }
+
+    private void Update()
+    {
+        // Setting current speed based on if player is sprinting
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = sprintSpeed;
+        }
+        else
+            speed = normalSpeed;
+        // Getting keyboard inputs
+        keyboardInput = (transform.right * Input.GetAxisRaw("Horizontal")) + (transform.forward * Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        //Moving player
+        playerRb.velocity = (keyboardInput * speed) + (new Vector3(0, playerRb.velocity.y, 0));
+
+        //Performing jump if player on the ground
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            playerRb.velocity = new Vector3(0, Input.GetAxis("Jump") * jumpForce, 0);
+        }
+    }
+
+
+    //Checking if colling with ground to set grounded
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+}
