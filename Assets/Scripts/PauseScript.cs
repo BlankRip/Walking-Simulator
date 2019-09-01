c0d5bfce68da3ffcf3bc87cdea4f3691cf5ad255
Aniketh <a.gondesi@student.saedubai.com>
2019-09-01 13:52:30 +0400

    Pause screen was added.

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    [SerializeField] GameObject pauseScreen;
+    bool paused = false;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!paused)
+            {
+                Time.timeScale = 0;
+                pauseScreen.SetActive(true);
+                paused = true;
+            }
+            else if(paused)
+            {
+                pauseScreen.SetActive(false);
+                Time.timeScale = 1;
+                paused = false;
+            }
+
+        }
+    }
+}
