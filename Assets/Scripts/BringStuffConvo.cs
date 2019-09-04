78618fb2445d81c1239852ded920528dc964b3da
Aniketh <a.gondesi@student.saedubai.com>
2019-09-04 12:40:25 +0400

    Added all Background music but still need sound effects.

--- Assets/Scripts/BringStuffConvo.cs
+++ Assets/Scripts/BringStuffConvo.cs
@@ -9,6 +9,14 @@ public class BringStuffConvo : MonoBehaviour
     [HideInInspector] public bool collectableNow;            // To check if the bushes were collectable
     [HideInInspector] public int collected;                  // A counter to keep count of how many bushes are collected so far
     [SerializeField] int ColletXNumber;                      // Number of bushes are needed to be collect to enter the village
+    AudioSource soundSource;                                 // The audio source
+    [SerializeField] AudioClip changBGS;                     // The clip to which the background music is changed to
+
+
+    private void Start()
+    {
+        soundSource = FindObjectOfType<AudioSource>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -23,6 +31,8 @@ public class BringStuffConvo : MonoBehaviour
             else if (collected >= ColletXNumber)
             {
                 enterIn.SetActive(true);
+                soundSource.clip = changBGS;
+                soundSource.Play();
                 Destroy(gameObject);
             }
         }
